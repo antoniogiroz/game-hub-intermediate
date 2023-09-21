037e2d407ff33cb313e98f15ffadc757fe098083
Antonio Gil <agil@fmit-solutions.com>
2023-09-21 21:10:50 +0200

    feat: add app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { NavBar } from "./components/nav-bar";
+
 export default function App() {
   return (
     <>
-      <h1>Game Hub</h1>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
+
+        <GridItem area="main">Main</GridItem>
+      </Grid>
     </>
   );
 }
