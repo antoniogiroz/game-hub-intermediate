caef3e2d9975c6e402f6b2a7e02436d4010a1e57
Antonio Gil <agil@fmit-solutions.com>
2023-09-22 07:55:14 +0200

    feat: load games from the API

---
+++ src/hooks/use-games.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: string;
+  name: string;
+}
+
+interface GamesResponse {
+  count: number;
+  results: Game[];
+}
+
+export function useGames() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<GamesResponse>("/games", { signal: controller.signal })
+      .then((response) => setGames(response.data.results))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+
+        setError(error.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+}
